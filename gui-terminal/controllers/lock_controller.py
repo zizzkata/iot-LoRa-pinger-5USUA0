23237433f6b8faa9ff12d76525a9c1f8f4bbd5a7
Zdravko Zdravkov <zdravko4d@gmail.com>
2023-01-17 14:18:56 +0100

    Change user extraction method

--- gui-terminal/controllers/lock_controller.py
+++ gui-terminal/controllers/lock_controller.py
@@ -2,17 +2,24 @@ from utils.async_wrapper import threaded_task
 import utils.file_utils as fu
 import utils.hashing_utils as hu
 
+user = None
 
 def unlock_screen(pass_code: str, callback):
-    def threaded_func():
-        try:
-            user = fu.get_user()
-            if user:
-                callback(user["pass_code"] == hu.hash_string(pass_code))
-            else:
-                raise Exception("No user found")
-        except Exception as e:
-            print(e)
-            callback(False)
-        
-    threaded_task(threaded_func, ())
+    global user
+    if user:
+        callback(user["pass_code"] == hu.hash_string(pass_code))
+    else:
+        print("No user found")
+        callback(False)
+
+     
+def import_user():
+    global user
+    try:
+        user = fu.get_user()
+        print("User found")
+    except Exception as e:
+        print("Failed to get the user")
+        print(e)
+
+threaded_task(import_user, ())
